8e03e692265f8cd06b662c75405453f67c0782cb
pranshuambwani <47500792+pranshuambwani@users.noreply.github.com>
2019-08-22 13:57:21 +0530

    Update StoreFrontJDBC.java

--- JDBC/StoreFrontJDBC.java
+++ JDBC/StoreFrontJDBC.java
@@ -7,13 +7,28 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-/**
- * This class performs certain queries on database Store Front
- * @author Twinkle-Garg
- *
- */
-public class StoreFrontJDBC {
 
+public class StoreFrontJDBC {
+	
+	 /**
+     * This method establishes connection with database
+     * @return connection instance
+     */
+    private static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/StoreFront", "root", "root");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(2);
+        }
+        return conn;
+    }
+    
     /**
      * This method:
      * Given the id of a user, fetch all orders (Id, Order Date, Order Total) of that user which are in shipped state.
@@ -21,18 +36,11 @@ public class StoreFrontJDBC {
      * @param userId
      */
     public void fetchOrders(int userId) {
-        String query = "select DISTINCT o.id, o.date_of_order_placed, o.amount from user u join orders o join ";
-        query += "item_list i join product p where u.id=o.user_id AND o.id=i.order_number AND ";
-        query += "o.id NOT IN(Select order_number from item_list where ";
-        query += "status='Cancelled' OR status='Returned') AND ";
-        query += "p.id=i.product_id AND u.id=? ORDER BY o.date_of_order_placed DESC";
-
+        String query = "select o.id,o.date_of_order_placed,o.amount from orders o join item_list i on o.id=i.order_number where  o.user_id=? and i.status = 'SHIPPED' order by o.date_of_order_placed";
         try {
             Connection conn = getConnection();
             PreparedStatement stmt = conn.prepareStatement(query);
             stmt.setInt(1, userId);
-            System.out.println("The SQL query is: " + query);
-            System.out.println();
             ResultSet rset = stmt.executeQuery();
             List<Orders> listOfOrders = new ArrayList<>();
 
@@ -54,7 +62,7 @@ public class StoreFrontJDBC {
         }
 
     }
-
+    
     /**
      * This method inserts images of a product using batch insert technique
      * @param productId
@@ -85,14 +93,14 @@ public class StoreFrontJDBC {
         }
 
     }
-
+    
     /**
      * This method change status as inactive of all those products which were not ordered by any Shopper in last 1 year.
      * Return the number of inactive products.
      * @return
      */
     public int setInactive() {
-        String query = "update product set status='Inactive' WHERE id NOT IN(SELECT i.product_id FROM item_list i join orders o where"
+        String query = "update product set active=0 WHERE id NOT IN(SELECT i.product_id FROM item_list i join orders o where"
                 + " o.id=i.order_number AND DATEDIFF(CURDATE(),o.date_of_order_placed)<365)";
         int numOfInactives = 0;
         try {
@@ -109,13 +117,14 @@ public class StoreFrontJDBC {
         return numOfInactives;
     }
 
+    
     /**
      * This method:
      * Select and display the category title of all top parent categories
      * sorted alphabetically and the count of their child categories.
      */
     public void showCategories(){
-        String query="select DISTINCT c.parent_category, COUNT(c.parent_id) AS Numbber_Of_Children FROM category c GROUP BY c.parent_id ORDER BY c.parent_category";
+        String query="select p.category_name,count(c.parent_id) from category p left join category c on p.id = c.parent_id group by p.id";
         try {
             
             Connection conn = getConnection();
@@ -124,7 +133,7 @@ public class StoreFrontJDBC {
             ResultSet rset = stmt.executeQuery();
             List<Category> parentList=new ArrayList<>();
             while (rset.next()) {
-                parentList.add(new Category(rset.getString("parent_category"),rset.getInt("Numbber_Of_Children")));
+                parentList.add(new Category(rset.getString(1),rset.getInt(2)));
             }
             
             for(Category category:parentList){
@@ -134,23 +143,6 @@ public class StoreFrontJDBC {
             ex.printStackTrace();
         }
     }
-    
-    /**
-     * This method establishes connection with database
-     * @return connection instance
-     */
-    private static Connection getConnection() {
-        Connection conn = null;
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/storefront", "root", "root");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            System.exit(1);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.exit(2);
-        }
-        return conn;
-    }
+
+
 }
