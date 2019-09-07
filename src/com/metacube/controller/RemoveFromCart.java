cb93b6051e8e5aa3357b57ea5b81b7f0835feab8
pranshuambwani <47500792+pranshuambwani@users.noreply.github.com>
2019-09-07 14:08:37 +0530

    Update RemoveFromCart.java

--- src/com/metacube/controller/RemoveFromCart.java
+++ src/com/metacube/controller/RemoveFromCart.java
@@ -6,8 +6,8 @@ import com.metacube.facade.Facade;
 public class RemoveFromCart {
 	
 	public static void RemoveFromCartReq(int cart_id,int proID){
-		boolean isItemRemoved = Facade.removeItem(cart_id, proID);
-		if(isItemRemoved)
+		boolean ItemRemoved = Facade.removeItem(cart_id, proID);
+		if(ItemRemoved)
 			System.out.println("Item Removed Successfully");
 		else
 			System.out.println("Unable to remove item!!Please try again");
